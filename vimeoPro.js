a19c15aa2789242b304726996947498e01b3c4d6
WILL MEIER <willmeierart@gmail.com>
2017-05-26 01:22:57 -0600

    cleaner code

--- vimeoPro.js
+++ vimeoPro.js
@@ -4,35 +4,28 @@ $(function() {
         initialize().promise().done(scrollAction())
     })
 
-    let
-        iframe$ = $('iframe'),
-        video$ = [],
-        playerID,
-        scrollTimer
+    let iframe$ = $('iframe'),
+        video$ = []
 
     for (let i of iframe$) {
         let url = i.src
         if (url.includes('vimeo')) {
             i.classList.add('vimeo')
-
         }
     }
-
 
     function initialize() {
         return $.each($('.vimeo'), function(video) {
             newPlayer($(this))
         })
-
     }
 
-    function newPlayer(i) {
-        i = new Vimeo.Player(i)
-        giveUniqueID(i)
-        initPresets(i)
-        video$.push(i)
+    function newPlayer(player) {
+        player = new Vimeo.Player(player)
+        giveUniqueID(player)
+        initPresets(player)
+        video$.push(player)
         return video$
-
     }
 
     function initPresets(player) {
@@ -67,7 +60,6 @@ $(function() {
             switch (error.name) {
                 case 'PasswordError':
                     break;
-
                 case 'PrivacyError':
                     break;
                 default:
@@ -107,33 +99,32 @@ $(function() {
     function giveUniqueID(player) {
         player.getVideoId().then(function(id) {
             player.element.id = id
-            playerID = player.element.id
-            player.classList.add('tryIt')
         }).catch(function(error) {})
-        return playerID
     }
 
     function scrollAction() {
+        let scrollTimer
         $(window).scroll(function() {
             if (scrollTimer) {
                 clearTimeout(scrollTimer)
             }
-            scrollTimer = setTimeout(handleScroll, 500)
+            scrollTimer = setTimeout(handleScroll, 200)
 
             function handleScroll() {
                 $.each(video$, function(i) {
+                    let thi$Video = $(this)[0]
                     if (isInView($(this.element)[0])) {
-                        console.log($(this)[0], 'hi')
-                        autoPlay($(this)[0])
+                        console.log(thi$Video, 'hi')
+                        autoPlay(thi$Video)
                     }else{
-                        console.log($(this)[0], 'bye')
-                        pause($(this)[0])
+                        console.log(thi$Video, 'bye')
+                        pause(thi$Video)
+                        soundOff(thi$Video)
                     }
                 })
             }
 
             function isInView(video) {
-                console.log(video)
                 let bounds = video.getBoundingClientRect()
                 return (
                     bounds.top >= 0 &&
